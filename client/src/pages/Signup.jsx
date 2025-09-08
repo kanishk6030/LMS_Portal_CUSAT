ecd0236f7a525db819af7e98aca14a4c14dd1f99
Kanishka Vats <kanishkvats2020@gmail.com>
2025-09-08 13:54:39 +0530

    Updated the frontend

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { SignupFormDemo } from '../components/SignupFormDemo'
-import { LoginForm } from '../components/LoginForm'
+
 
 function Signup() {
   return (
     <>
       <div className='relative w-full h-screen bg-background flex items-center justify-center'>
         <SignupFormDemo />
-        {/* <LoginForm /> */}
       </div>
     </>
   )
